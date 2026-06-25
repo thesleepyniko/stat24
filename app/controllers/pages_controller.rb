408ba7b74546b0eb7faf4c8d21d4c299e758af2e
Niko Yu <137782638+thesleepyniko@users.noreply.github.com>
2026-06-25 16:10:00 -0400

    misc: initial + landing page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+  def home
+    @photo = Rails.cache.read("random_photo")
+    if @photo == nil
+      @photo = ""
+    else
+      @author = @photo["author"]
+      @acft = @photo["airline"]
+      @photo = @photo["url"]
+    end
+  end
+end
---
+++ app/jobs/random_image_job.rb
@@ -0,0 +1,22 @@
+require "net/http"
+
+class RandomImageJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    uri = URI("https://tico09.com/photos/api/recent")
+
+    Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+      request = Net::HTTP::Get.new uri
+
+      response = http.request request # Net::HTTPResponse object
+      if response.is_a?(Net::HTTPSuccess)
+        body = response.body
+        body_json = JSON.parse(body)
+        photos = body_json["photos"]
+        random_photo = photos.sample
+        Rails.cache.write("random_photo", random_photo)
+      end
+    end
+  end
+end
